1a416360f11f4156258cbe09580ea55c28f0bc84
chankruze <chankruze@gmail.com>
2019-05-02 05:15:29 +0530

    One_Max: Initial Commit
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ omni_One_Max.mk
@@ -0,0 +1,21 @@
+# Release name
+PRODUCT_RELEASE_NAME := One_Max
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := UMIDIGI
+PRODUCT_DEVICE := One_Max
+PRODUCT_MANUFACTURER := UMIDIGI
+PRODUCT_MODEL := One_Max
+PRODUCT_NAME := omni_One_Max
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0
